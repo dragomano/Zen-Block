271b64f7be45ab71e93fbabe7bd4f807a9c7b2e0
Bomber Code <contacto@bombercode.net>
2019-04-13 15:25:08 -0300

    Spanish Latin translation!
    I understand that the utf8 version is unified and not, I say it by Russian version but you will tell me.

---
+++ Themes/default/languages/ZenBlock/.spanish_latin.php
@@ -0,0 +1,21 @@
+<?php
+/* Spanish translation https://www.bombercode.net Copyright 2014-2019 */
+$txt['zen_settings'] = 'Bloque Zen';
+$txt['zen_desc'] = 'Puede cambiar cualquier configuración del bloque con el contenido de la primera publicación aquí.';
+$txt['zen_block_enable'] = 'El primer contenido de la publicación.';
+$txt['zen_block_status'] = 'Estado por defecto';
+$txt['zen_block_status_set'] = array('Minimizado', 'Maximizado');
+$txt['zen_block_link'] = 'Enlace';
+$txt['zen_block_topic'] = 'Tema popular';
+$txt['zen_share_title'] = 'Compartir';
+$txt['zen_where_is'] = array('Desactivar', 'No mostrar en la primera página de temas', 'Mostrar en todas las páginas de temas');
+$txt['zen_ignored_boards'] = 'Foros ignorados';
+$txt['zen_ignored_boards_desc'] = 'Ingrese la ID de los foros donde no se mostrará el Bloque Zen.';
+$txt['zen_yashare'] = 'Mostrar bloque de enlace de botón "Yandex.Compartir"';
+$txt['zen_yashare_set'] = array(0 => 'Ninguno', 'botón' => 'Como botón', 'Enlace' => 'Como enlace', 'icono' => 'Iconos y menu', 'Ninguno' => 'Solo iconos');
+$txt['zen_yashare_title'] = 'Íàñòðîéêà áëîêà «Ïîäåëèòüñÿ» îò ßíäåêñà';
+$txt['zen_yashare_blocks'] = 'Iconos dentro del bloque "Yandex.Compartir"';
+$txt['zen_yashare_array'] = 'Iconos mostrados en el bloque derecho "Yandex.Compartir"';
+$txt['zen_yashare_icons'] = 'blogger, digg, evernote, delicious, diary, facebook, friendfeed, gplus, juick, liveinternet, linkedin, lj, moikrug, moimir, myspace, odnoklassniki, tutby, twitter, vkontakte, yaru, yazakladki, surfingbird';
+$txt['zen_yashare_desc'] = 'Servicios soportados: ' . $txt['zen_yashare_icons'];
+?>
